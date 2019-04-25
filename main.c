647b6bef4bdda288f3dd1659af6ad567cfde778f
0984647853 <45658401+0984647853@users.noreply.github.com>
2019-04-25 14:13:08 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+#include<string.h>
+#include"screen.h"
+void findDuplicate(void);
+int main() {
+	FILE *f;
+	f = fopen("wlist.txt", "r");
+	if (f == NULL) {
+		printf("Can not open the file! \n");
+	}
+	else printf("Open successfully \n");
+	char content[255];
+	int Number_Of_Start_Letter[26]={0}; // array contains 26 Alphabet, use for counting how many words which start with each letters
+	int Num_Letter_in_file[26]={0}; // array contains 26 Alphabet, use for counting how much times each letter appear in file
+	int a;
+	int x=0;
+	while(fscanf(f, "%s", content) != EOF) {
+		a = content[0]; //take the first letter of the word and convert it into Decimal value 
+		Number_Of_Start_Letter[a-65]++; // counting the frequency of the appearance 
+		int c=0;
+		while(content[c] != '\0'){
+			if (content[c] >= 'A' && content[c] <= 'Z') {
+				x = content[c] - 'A';
+				Num_Letter_in_file[x]++;
+			}
+		c++;
+		}
+	}
+    fclose(f);
+	clearScreen();
+	findDuplicate();
+	barChart(Number_Of_Start_Letter,Num_Letter_in_file);
+	resetColors();
+	printf("\033[1;60H");
+}
+void findDuplicate(){
+	FILE *fp;
+	fp = fopen("wlist.txt", "r");
+	int i = 0;
+	char s[255]; 
+	int N=0;
+	while(fscanf(fp,"%s",s) != EOF){N++;}
+	char strings[255][10];
+
+	rewind(fp);
+	for(i=0;i<N;i++){
+		fscanf(fp,"%s",s);
+		strcpy(strings[i], s);
+	}
+	printf("\033[4;6H");
+	setColors(YELLOW,bg(BLACK));
+	printf("These words are appear multiple times in the file: ");
+	int count=0;
+	for(int j=0;j<N;j++){
+		for(int z=j+1;z<N;z++)
+		{
+			if(!strcmp(strings[j],strings[z])) 
+			{
+				printf("%s ",strings[j]);
+				break;
+			}
+		}
+	}
+	resetColors();
+}
+
---
+++ screen.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include"screen.h"
+void barChart(int a[],int b[])
+{
+	int i, j; //counters
+	setColors(CYAN, bg(BLACK));
+	for(i=0;i<26;i++){
+		for(j=0;j<=a[i];j++){
+			printf("\033[%d;%dH",60-j,i*3);
+#ifdef UNICODE		//conditional compilation
+			printf("%s","\u2590");
+			if((j==a[i]-1)&&(j!=0))
+				{
+					printf("\033[%d;%dH",60-j-2,i*3);
+					printf("%d",a[i]);
+				}	
+#else
+			printf("%c",' ');
+#endif
+		}
+	}
+	setColors(GREEN,bg(BLACK));
+	for(i=0;i<26;i++){
+		for(j=0;j<=b[i]/(3);j++){
+			printf("\033[%d;%dH",60-j,i*3+85);
+#ifdef UNICODE		//conditional compilation
+			printf("%s","\u2590");
+			if((j==b[i]/(3)-1)&&(j!=0))
+			{
+				printf("\033[%d;%dH",60-j-2,i*3+85);
+				printf("%d",b[i]);
+			}	
+#else
+			printf("%c",' ');
+#endif
+		}
+	}
+}
+void clearScreen(void)
+{
+	printf("\033[2J");
+	fflush(stdout);		//output the escape sequence immediately
+}
+
+void setColors(short fg, short bg)
+{
+	printf("\033[%d;%d;1m",fg,bg);
+	fflush(stdout);
+}
+void resetColors(void)
+{
+	printf("\033[0m");
+	fflush(stdout);
+}
